0fd7cbda0209bd88ff61988e4f07cec106e3a34d
Julia Gerlach <juliagerlach442@gmail.com>
2018-06-20 15:15:06 -0500

    addedNodeList class

--- BinarySearchTree/NodeList.cs
+++ BinarySearchTree/NodeList.cs
@@ -1,6 +1,33 @@
-﻿namespace BinarySearchTree
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
 {
-    public class NodeList<T>
+    public class NodeList<T> : Collection<Node<T>>
     {
+        public NodeList() : base() { }
+
+        public NodeList(int initialSize)
+        {
+        //Add the specified number of items 
+        for (int i = 0; i < initialSize; i++)
+            base.Items.Add(default(Node<T>));
+        }
+
+        public Node<T> FindByValue(T value)
+        {
+            IEnumerable<Node<T>> Items = null;
+            //search the list for the value
+            foreach (Node<T> node in Items)
+                if (node.Value.Equals(value))
+                    return node;
+
+            //if we reached here, we didn't find a matching node
+            return null;
+        }
     }
 }
