bf643871dfce7d5fb85a1934066e196e56531892
Julia Gerlach <juliagerlach442@gmail.com>
2018-06-20 15:46:10 -0500

    binarytree class added

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree<T>
+    {
+        private BinaryTreeNode<T> root;
+        
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public virtual void Clear()
+        {
+            root = null;
+        }
+
+        public BinaryTreeNode<T> Root
+        {
+            get
+            {
+                return root;
+            }
+            set
+            {
+                root = value;
+            }
+        }
+    }
+}
